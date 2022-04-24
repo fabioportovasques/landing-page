2502a1c0d3ac51d524a59e8783261b01ffc748d0
fabioportovasques <fabioportovasques321@gmail.com>
2022-04-24 02:24:27 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,108 @@
+<html>
+    <head>
+
+        <meta charset="UTF-8">
+        <title>Landing Page</title>
+        
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+    </head>
+    <body>
+
+
+    <header>
+        <div class="container">
+
+             <div class="logo">
+                Meu Logo
+            </div>
+
+
+            
+                <div class="menu">
+                    <nav>
+                        <ul>
+                            <li><a href="">home</a></li>
+                            <li><a href="">home</a></li>
+                            <li><a href="">home</a></li>
+                            <li><a href="">home</a></li>
+                            <li><a href="">home</a></li>
+                            <li><a href="">home</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            
+
+          
+        </div>
+    </header>
+
+    
+
+   
+
+    <section id="banner">
+        <div class="container banner">
+            <div class="texto_esqu">
+                <strong>Um pouco da história da raça</strong>
+            is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </div>
+            <div class="img">
+                    <img src="assets/img/boxer.png" width="340px" height="320px">
+            </div>
+        </div>
+    </section>
+
+    <section id="secundaria">
+        <section class="container secundaria">
+
+            <article>
+                <div class="circulo1">
+
+                </div>
+                <div class="texto1"><strong>Lorem Ipsum</strong></div>
+                <div class="texto1-1">typesetting, remaining essentially unchanged.It was popularised in the 1960s with
+                     the release of Letraset sheets containing Lorem Ipsum passages, 
+                </div>
+            </article>
+            
+                
+            <article>
+            <div class="circulo2">
+
+            </div>
+            <div class="texto2"><strong>Lorem Ipsum</strong></div>
+            <div class="texto2-2">typesetting, remaining essentially unchanged.It was popularised in the 1960s with
+                     the release of Letraset sheets containing Lorem Ipsum passages, 
+            </div>
+            </article>
+
+            <article>
+            <div class="circulo3">
+
+            </div>
+            <div class="texto3"><strong>Lorem Ipsum</strong></div>
+            <div class="texto3-3">typesetting, remaining essentially unchanged.It was popularised in the 1960s with
+                     the release of Letraset sheets containing Lorem Ipsum passages, 
+                </div>
+            </article>
+
+            
+        </section>
+
+    </section>
+
+    <footer id="footer">
+        <div class="container footer">
+             <div class="footer-esqu">
+                   <strong>Where does it come from</strong>
+             </div>
+
+             <div class="footer-dir">
+             <strong>E-mail:fabioportovasques321@gmail.com</strong>
+             </div>
+        </div>
+    </footer>
+
+    </body>
+</html>
